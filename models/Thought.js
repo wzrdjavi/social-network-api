ab8e3c45ee80c53f95a2f02c7a34241f2acb445d
wzrdjavi <jmaster112605@gmail.com>
2022-11-02 21:29:22 -0500

    Thought javascript code filled

---
+++ models/Thought.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required:true,
+            max_length: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+         },
+         id: false,
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function (){
+    return this.reactions.length;
+});
