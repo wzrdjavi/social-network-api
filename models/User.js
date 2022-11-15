774ae6c378f85f0cc1b5b67dbbd1b2a0e5aa1056
wzrdjavi <jmaster112605@gmail.com>
2022-11-15 15:39:15 -0600

    initial user.js code

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema,model } = require("mongoose");
+
+const userSchema = new Schema (
+    {
+        username:{
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],
+        },
+        thoughts: [
+            {
+              type: Schema.Types.ObjectId,
+              ref: 'Thought',
+            },
+          ],
+          friends: [
+            {
+              type: Schema.Types.ObjectId,
+              ref: 'User',
+            },
+          ],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this. friends.length;
+})
+
+const User = model('User',userSchema);
+
+module.exports = User;
